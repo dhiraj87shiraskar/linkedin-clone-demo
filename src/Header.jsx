ed20c373fd871e3b24a68ed12fabbe1e014058d0
DESKTOP-9E32TLL\Admin <dhiraj.k.shiraskar@gmail.com>
2021-07-28 01:12:54 +0530

    added initial app setting header left ui

---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="" />
+                <div className="header__search">
+                    <SearchIcon />
+                    <input  type="text" />
+                </div>
+
+            </div>
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
