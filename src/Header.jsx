35d3381fb7e4181620dca64556277ba06b2e1dda
DESKTOP-9E32TLL\Admin <dhiraj.k.shiraskar@gmail.com>
2021-07-28 09:22:29 +0530

    added initial app setting header right partaily UI

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import './Header.css'
+import HeaderOption from './HeaderOption';
 
 function Header() {
     return (
@@ -14,7 +17,8 @@ function Header() {
 
             </div>
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
             </div>
         </div>
     )
